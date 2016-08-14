bcb5b3b5bd8090854f71cd85fcc09716bc400862
Maciej <abckermit@gmail.com>
2016-08-14 22:30:41 +0200

    Drobne poprawki w widoku edycji - formularz

--- src/app/control-panel/house-edit/house-edit.component.ts
+++ src/app/control-panel/house-edit/house-edit.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs/Rx";
-import {FORM_PROVIDERS} from "@angular/common";
-import {FormArray, FormGroup, FormControl, Validators, FormBuilder, REACTIVE_FORM_DIRECTIVES} from "@angular/forms";
+import {FORM_PROVIDERS, Location} from "@angular/common";
+import {FormGroup, Validators, FormBuilder, REACTIVE_FORM_DIRECTIVES} from "@angular/forms";
 
 import {ControlPanelService} from "../control-panel.service";
 import {House} from "../../shared/house";
@@ -24,7 +24,7 @@ export class HouseEditComponent implements OnInit, OnDestroy {
   constructor(private route:ActivatedRoute,
               private service:ControlPanelService,
               private formBuilder:FormBuilder,
-              private router:Router) {
+              private location: Location) {
   }
 
   ngOnInit() {
@@ -48,10 +48,6 @@ export class HouseEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  private navigateBack() {
-    this.router.navigate(['../']);
-  }
-
   onSubmit() {
     const newHouse = this.houseForm.value;
     if (this.isNew) {
@@ -59,50 +55,27 @@ export class HouseEditComponent implements OnInit, OnDestroy {
     } else {
       this.service.editHouse(this.house, newHouse);
     }
-    this.navigateBack();
+    this.location.back();
   }
 
   onCancel() {
-    this.navigateBack();
-  }
-
-  onAddTempSensor(value:string, xPos:string) {
-    (<FormArray>this.houseForm.controls['tempSensors']).push(
-      new FormGroup({
-        value: new FormControl(value, Validators.required),
-        xPos: new FormControl(xPos, [Validators.required, Validators.pattern("\\d+")])
-      })
-    )
-  }
-
-  onRemoveTempSensor(index:number) {
-    (<FormArray>this.houseForm.controls['tempSensors']).removeAt(index);
+    this.location.back();
   }
 
   private initForm() {
     let houseName = '';
     let houseOwner = '';
     let houseImgUrl = '';
-    let houseTempSensors:FormArray = new FormArray([]);
 
     if (!this.isNew) {
       houseName = this.house.name;
       houseOwner = this.house.owner;
       houseImgUrl = this.house.imagePath;
-      for (let i = 0; i < this.house.tempSensors.length; i++) {
-        houseTempSensors.push(
-          new FormGroup({
-            value: new FormControl(this.house.tempSensors[i].value, Validators.required),
-            xPos: new FormControl(this.house.tempSensors[i].xPos, [Validators.required, Validators.pattern("\\d+")])
-          })
-        );
-      }
     }
     this.houseForm = this.formBuilder.group({
       name: [houseName, Validators.required],
       owner: [houseOwner, Validators.required],
       imagePath: [houseImgUrl, Validators.required],
-      tempSensors: houseTempSensors
     });
   }
 }
